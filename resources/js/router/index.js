0c7f25a03b9a83e64716f8c0c458589724c3ce05
Vicky Valerian <vickyvalerian27@gmail.com>
2024-09-11 16:11:13 +0800

    delete about page import in router

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,7 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import home from "../components/HomePage.vue";
-import about from "../components/AboutPage.vue";
 import notFound from "../components/NotFoundPage.vue";
 
 const routes = [
